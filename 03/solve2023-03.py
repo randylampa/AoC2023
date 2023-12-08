b336a71e23e6182a1024330d26644c0e6325de2a
René Panák <rene.panak@seznam.cz>
2023-12-08 07:38:18 +0100

    solve day 3 B

--- 03/solve2023-03.py
+++ 03/solve2023-03.py
@@ -12,6 +12,7 @@ par_dir = os.path.dirname(cur_dir)
 sys.path.append(par_dir)
 import utils
 import re
+from functools import reduce
 
 YEAR = 2023
 DAY = 3
@@ -110,7 +111,80 @@ def solve_part_2(demo:bool) -> str:
 	fl = cur_dir + '/' + fn
 	"""Do something here >>>"""
 
-	answer = None
+	lines = utils.read_file_into_list(fl)
+	# ~ print(lines)
+
+	rgx = re.compile(r'[\d\.]')
+
+
+	partnumMap = {}
+	def setPartnum(idxLine:int, idxCol:int, symbol:str, partnum:int):
+		key = '{}-{}-{}'.format(idxLine, idxCol, symbol)
+		if not key in partnumMap:
+			partnumMap[key] = []
+		partnumMap[key].append(partnum)
+
+
+	lineidx = 0
+	lineidxMax = len(lines)
+	for line in lines:
+		# ~ print(line)
+		for m in re.finditer(r'\d+', line):
+			# ~ print(m)
+			sidx = int(m.start())
+			eidx = int(m.end())
+			lab = m.group()
+			pnlab = int(lab)
+			print('LINE {}: PARTNUM {} from {} to {}'.format(lineidx, lab, sidx, eidx))
+			# ~ print(line[sidx])
+			# ~ print(line[eidx])
+			idxL = sidx-1 if sidx>0 else None
+			idxR = eidx if eidx<len(line) else None
+			# ~ print('  idxL:{}, idxR:{}'.format(idxL,idxR))
+			if idxL and line[idxL]!='.':
+				print('  has SYMBOL`{}` on the left'.format(line[idxL]))
+				setPartnum(lineidx, idxL, line[idxL], pnlab)
+				continue
+			if idxR and line[idxR]!='.':
+				print('  has SYMBOL`{}` on the right'.format(line[idxR]))
+				setPartnum(lineidx, idxR, line[idxR], pnlab)
+				continue
+			idxL = idxL if idxL is not None else sidx
+			idxR = idxR if idxR is not None else eidx
+			if lineidx>0:
+				# ~ has prev line
+				idxLineTop = lineidx - 1
+				strip = lines[idxLineTop][idxL:idxR+1]
+				print(' strip Top', strip)
+				symbols = rgx.sub('', strip)
+				if len(symbols)>0:
+					print('  has SYMBOL`{}` on top'.format(symbols))
+					setPartnum(idxLineTop, idxL + strip.index(symbols), symbols, pnlab)
+					continue
+			if lineidx<lineidxMax-1:
+				# ~ has next line
+				idxLineBottom = lineidx + 1
+				strip = lines[idxLineBottom][idxL:idxR+1]
+				print(' strip Bottom', strip)
+				symbols = rgx.sub('', strip)
+				if len(symbols)>0:
+					print('  has SYMBOL`{}` on bottom'.format(symbols))
+					setPartnum(idxLineBottom, idxL + strip.index(symbols), symbols, pnlab)
+					continue
+
+		lineidx = lineidx+1
+
+	print('partnumMap', partnumMap)
+
+	gearRatios = []
+	for key in partnumMap.keys():
+		vals = partnumMap[key]
+		if key[-1]=='*' and len(vals)==2:
+			ratio = reduce((lambda x,y:x*y), vals)
+			# ~ print('ratio for key {} is {}'.format(key, ratio))
+			gearRatios.append(ratio)
+
+	answer = sum(gearRatios)
 
 	"""<<< Do something here"""
 	utils.print_answer(2, demo, answer)
@@ -118,11 +192,13 @@ def solve_part_2(demo:bool) -> str:
 
 def main():
 
-	solve_part_1(0)
+	# ~ solve_part_1(0)
 	# ~ Answer_1 = 4361 (demo)
 	# ~ Answer_1 = 543867 is right
 
-	# ~ solve_part_2(1)
+	solve_part_2(0)
+	# ~ Answer_2 = 467835 (demo)
+	# ~ Answer_2 = 79613331 is right
 
 	pass
 
