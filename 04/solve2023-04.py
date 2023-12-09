bb93b024a78ed517990024e2dc4ca872a6a6da03
René Panák <rene.panak@seznam.cz>
2023-12-09 12:23:19 +0100

    solve day 4.1 - desc 4.2

--- 04/solve2023-04.py
+++ 04/solve2023-04.py
@@ -18,6 +18,20 @@ DAY = 4
 ISSUE = '04'
 
 
+def parse_card(line:str)->dict:
+	name, snumsAll = line.split(':')
+
+	snwin, snums = snumsAll.split('|')
+
+	numsWin = [*map(int, re.findall(r'\d+', snwin))]
+	nums = [*map(int, re.findall(r'\d+', snums))]
+
+	return {
+		'name': name,
+		'numsWin': numsWin,
+		'nums': nums
+	}
+
 
 '''
 	SOLVE PART 1
@@ -29,7 +43,26 @@ def solve_part_1(demo:bool) -> str:
 	fl = cur_dir + '/' + fn
 	"""Do something here >>>"""
 
-	answer = None
+	lines = utils.read_file_into_list(fl)
+	# ~ print(lines)
+
+	cards_values = []
+
+	for line in lines:
+		card = parse_card(line)
+		# ~ print(card)
+		value = None
+		for n in card['nums']:
+			try :
+				card['numsWin'].index(n)
+				value = value*2 if value is not None else 1
+			except ValueError:
+				# ~ print('number {} is not in winning'.format(n))
+				pass
+		if value is not None:
+			cards_values.append(value)
+
+	answer = sum(cards_values)
 
 	"""<<< Do something here"""
 	utils.print_answer(1, demo, answer)
@@ -54,9 +87,11 @@ def solve_part_2(demo:bool) -> str:
 
 def main():
 
-	solve_part_1(1)
+	solve_part_1(0)
+	# ~ Answer_1 = 13 (demo)
+	# ~ Answer_1 = 22193 is right
 
-	solve_part_2(1)
+	# ~ solve_part_2(1)
 
 	pass
 
